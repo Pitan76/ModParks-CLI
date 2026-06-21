a4f06673947aeb6858599e8eb7ac19fc43b814fa
PTOM76 <ptms76@outlook.jp>
2026-06-21 22:47:05 +0900

    bump version 0.0.3

--- dist/homebrew/modparks-cli.rb
+++ dist/homebrew/modparks-cli.rb
@@ -1,7 +1,7 @@
 ﻿class ModparksCli < Formula
   desc "CLI for ModParks"
   homepage "https://github.com/Pitan76/ModParks-CLI"
-  version "0.0.2"
+  version "0.0.3"
   
   if OS.mac?
     url "https://github.com/Pitan76/ModParks-CLI/releases/download/v0.0.1/modparks-cli-v0.0.1-macos-x86_64.tar.gz"
