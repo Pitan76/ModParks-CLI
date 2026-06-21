309b8e9c5c2ec289ed8032d3bac54d035a1d82e2
PTOM76 <ptms76@outlook.jp>
2026-06-21 17:29:47 +0900

    feat: add release distribution configurations for Homebrew, MacPorts, Winget, and WiX installer

---
+++ dist/homebrew/modparks-cli.rb
@@ -0,0 +1,21 @@
+﻿class ModparksCli < Formula
+  desc "CLI for ModParks"
+  homepage "https://github.com/Pitan76/ModParks"
+  version "0.1.0"
+  
+  if OS.mac?
+    url "https://github.com/Pitan76/ModParks/releases/download/v0.1.0/modparks-cli-v0.1.0-macos-x86_64.tar.gz"
+    sha256 "REPLACE_WITH_SHA256"
+  elsif OS.linux?
+    url "https://github.com/Pitan76/ModParks/releases/download/v0.1.0/modparks-cli-v0.1.0-ubuntu-x86_64.tar.gz"
+    sha256 "REPLACE_WITH_SHA256"
+  end
+
+  def install
+    bin.install "modparks-cli"
+  end
+
+  test do
+    system "#{bin}/modparks-cli", "--version"
+  end
+end
