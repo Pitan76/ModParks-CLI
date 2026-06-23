62d30acf3b118ba4253c0c10a254500dc806773c
PTOM76 <ptms76@outlook.jp>
2026-06-23 23:28:38 +0900

    chore: add release workflows and package definitions for Homebrew and MacPorts

--- dist/homebrew/modparks-cli.rb
+++ dist/homebrew/modparks-cli.rb
@@ -4,11 +4,11 @@ class ModparksCli < Formula
   version "0.0.9"
   
   if OS.mac?
-    url "https://github.com/Pitan76/ModParks-CLI/releases/download/v0.0.1/modparks-cli-v0.0.1-macos-x86_64.tar.gz"
-    sha256 "REPLACE_WITH_SHA256"
+    url "https://github.com/Pitan76/ModParks-CLI/releases/download/v0.0.9/modparks-cli-v0.0.9-macos-x86_64.tar.gz"
+    sha256 "d9465dc9e2840f5ad1a67cdb55e5695eb892bc40a2fc8d657dbdb819ba3ac3c8"
   elsif OS.linux?
-    url "https://github.com/Pitan76/ModParks-CLI/releases/download/v0.0.1/modparks-cli-v0.0.1-ubuntu-x86_64.tar.gz"
-    sha256 "REPLACE_WITH_SHA256"
+    url "https://github.com/Pitan76/ModParks-CLI/releases/download/v0.0.9/modparks-cli-v0.0.9-ubuntu-x86_64.tar.gz"
+    sha256 "a8f4450d5e2cf7b20917007a4414b0cc703f2b70d9a33cfd231cd1d29b814967"
   end
 
   def install
